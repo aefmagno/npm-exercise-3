63a4bf291e2f23faee80bca0bbf8419b65d79fe4
Aaron Magno <aefmagno@gmaik.com>
2022-10-11 09:26:41 +0800

    added prettier linter

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const { parse, resolve, toString, toArray } = require('@estilles/expression-parser');
+const {
+  parse,
+  resolve,
+  toString,
+  toArray,
+} = require('@estilles/expression-parser');
 
 const { argv } = require('node:process');
 
-const result = argv.length - 2 === 1 ? toString(parse(argv[2])) : 'Please Input A Single Argument';
+const result =
+  argv.length - 2 === 1
+    ? toString(parse(argv[2]))
+    : 'Please Input A Single Argument';
 
 console.log(result);
